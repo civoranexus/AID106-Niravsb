58b0c2a0cad8ebf3afe1a02da7f8527470218950
Niravsb <bordenirav@gmail.com>
2026-01-06 23:54:58 +0530

    added basic header to the website

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='  bg-purple-400 min-h-screen px-10 py-32'>
-        <div className='flex justify-center '>
-            <h1 className='text-3xl font-bold text-white tracking-wide mb-60'> ChatBot</h1>
-             
+    
+
+    <div className='  min-h-screen '>
+      {/*-----header----- */}
+      <div className='mb-20 flex justify-between'>
+        <div className='p-3 space-x-3 px-3 '>
+          <a href="#" className='text-xl font-bold text-gray-900'>AI ChatBot</a>
+          <a href="#" className='text-gray-700'>About</a>
+          <a href="#" className='text-gray-700'>Contact Me</a>
         </div>
-        <div className='flex justify-center mx-auto'>
-            <input placeholder="Let's Chat " className='bg-purple-200 flex-1 items-center text-center rounded-lg py-20 '></input>
+        <div className='p-3 space-x-3 px-3 items-center'>
+          <a href="#" className='inline-block px-4 py-2 bg-red-600 rounded-xl text-white font-bold shadow hover:bg-red-500 hover:shadow-xl transition duration-300'>Sign Up</a>
+          <a href="#" className='inline-block px-4 py-2 bg-blue-600 rounded-xl text-white font-bold hover:shadow-xl transition duration-300 hover:bg-blue-500'>Login</a>
+        </div>
+      </div>
+
+        
+        
+        
+        <div className='flex justify-center '>
+            <h1 className='text-3xl font-bold tracking-wide mb-60'> ChatBot</h1>
+             
+    
         </div>
        
         
